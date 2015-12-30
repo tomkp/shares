2d9e7c88e6bcea3a0070272f099301ec58a2892b
Tom Kemp-Potter <tom.kemp-potter@db.com>
2015-12-30 14:08:33 +0000

    ...

--- src/Application.js
+++ src/Application.js
@@ -20,14 +20,6 @@ var Application = React.createClass({
 
     fetch() {
         console.info('Application.fetch');
-        /*jsonp('http://www.google.com/finance/info?q=LON:ISF,LON:EMG,LON:LMI,LON:BRWM,LON:SSE,LON:MTC,LON:RDSA,NASDAQ:AAPL,LON:BLT,LON:TSCO,LON:MKS,tranLON:ULVR,LON:GSK,LON:RB,LON:JII,LON:TEM', null, (err, data) => {
-            if (err) {
-                console.error(error.message);
-            } else {
-                this.setState({quotes: data});
-            }
-            setTimeout(() => this.fetch(), 60000);
-        });*/
         axios.get('http://query.yahooapis.com/v1/public/yql', {
                 params: {
                     q: 'select * from yahoo.finance.quotes where symbol in ("ISF.L","EMG.L","LMI.L","BRWM.L","SSE.L","MTC.L","RDSA.L","BLT.L","TSCO.L","MKS.L","ULVR.L","GSK.L","RB.L","JII.L","TEM.L", "AAPL")',
@@ -39,6 +31,7 @@ var Application = React.createClass({
                 const quotes = response.data.query.results.quote;
                 console.info('quotes:', quotes);
                 this.setState({quotes: quotes});
+                setTimeout(() => this.fetch(), 60000);
             })
             .catch((response) => {
                 console.error(response);
--- src/Quotes.js
+++ src/Quotes.js
@@ -5,13 +5,18 @@ import Quote from './Quote';
 export default ({ quotes }) => (
     <table className="quotes">
         <tbody>
-        {quotes.sort((a, b)=> {
-            const aPercentChange = a.PercentChange.substr(0, a.PercentChange.length -1);
-            const bPercentChange = b.PercentChange.substr(0, b.PercentChange.length -1);
-            return bPercentChange - aPercentChange;
-        }).map((quote) => {
-            return <Quote quote={quote} key={quote.Symbol} />
-        })}
+        {
+            quotes
+                .filter((quote) => quote.Open)
+                .sort((a, b)=> {
+                    const aPercentChange = a.PercentChange.substr(0, a.PercentChange.length -1);
+                    const bPercentChange = b.PercentChange.substr(0, b.PercentChange.length -1);
+                    return bPercentChange - aPercentChange;
+                })
+                .map((quote) => {
+                    return <Quote quote={quote} key={quote.Symbol} />
+                })
+            }
         </tbody>
     </table>
 );
