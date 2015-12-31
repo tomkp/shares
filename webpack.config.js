4a6f21dc9e9c33c0594b5fee9adc38d021d0adbd
Tom Kemp-Potter <tom.kemp-potter@db.com>
2015-12-31 10:31:37 +0000

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     ],
     output: {
         path: __dirname,
-        filename: 'dist/webpacked.js'
+        filename: 'app/dist/webpacked.js'
     },
     debug: true,
     devtool: 'source-map',
