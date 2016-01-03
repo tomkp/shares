5ed5beae4117a2ea05cfb7780fe4d36ee6b4def4
tomkp <tom@tomkp.com>
2016-01-03 14:34:45 +0000

    ...

--- src/Application.js
+++ src/Application.js
@@ -9,6 +9,7 @@ import Quotes from './Quotes';
 import fetchQuotes from 'yahoo-finance-quotes';
 import fetchHistorical from './FetchHistorical';
 
+import moment from 'moment';
 
 const symbols = ["ISF.L", "EMG.L", "LMI.L", "BRWM.L", "SSE.L", "MTC.L", "RDSA.L", "BLT.L", "TSCO.L", "MKS.L", "ULVR.L", "GSK.L", "RB.L", "JII.L", "TEM.L", "AAPL"];
 //const symbols = ["ISF.L", "EMG.L", "LMI.L"];
@@ -17,7 +18,7 @@ const symbols = ["ISF.L", "EMG.L", "LMI.L", "BRWM.L", "SSE.L", "MTC.L", "RDSA.L"
 var Application = React.createClass({
 
     fetch() {
-        console.info('Application.fetch');
+        console.info(new Date(), 'Application.fetch');
         fetchQuotes(symbols)
             .then((quotes) => {
                 this.setState({quotes: quotes});
@@ -35,12 +36,14 @@ var Application = React.createClass({
     },
 
     componentDidMount() {
-        console.info('Application.componentDidMount');
+        //console.info('Application.componentDidMount', this.props);
+        const symbolsX = this.props.location.query.symbols;
+        //console.info('', symbolsX);
         this.fetch();
     },
 
     render() {
-        console.info('Application.render', this.state);
+        //console.info('Application.render', this.state);
         const quotes = this.state.quotes;
         return (
             <Quotes quotes={quotes} />
