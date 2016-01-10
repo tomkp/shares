0703e114bf6b93b805568ee50d02116c250e4119
tomkp <tom@tomkp.com>
2016-01-10 15:08:46 +0000

    ...

--- src/Quotes.js
+++ src/Quotes.js
@@ -6,6 +6,15 @@ import percent from './Percent';
 
 export default ({ quotes }) => (
     <table className="quotes">
+        <thead>
+            <tr>
+                <th> </th>
+                <th> </th>
+                <th className="change">Change</th>
+                <th className="percent-change">% Change</th>
+                <th className="latest">Latest</th>
+            </tr>
+        </thead>
         <tbody>
         {
             quotes
