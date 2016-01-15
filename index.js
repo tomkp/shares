0f7bf8b44dfa70c859c6c16944af322b6178b72b
tomkp <tom@tomkp.com>
2016-01-15 08:07:44 +0000

    ...

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
+'use strict';
+
 const electron = require('electron');
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
-
-
-// Report crashes to our server.
-//electron.crashReporter.start();
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -26,7 +24,7 @@ app.on('ready', function() {
     mainWindow = new BrowserWindow({width: 800, height: 600});
 
     // and load the index.html of the app.
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
 
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
