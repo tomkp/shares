9c4505d04daf9e735fb35645b5d5b7a86ede0073
tomkp <tom@tomkp.com>
2016-04-15 17:43:21 +0100

    ...

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({width: 375, height: 667});
 
     // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/app/index.html');
